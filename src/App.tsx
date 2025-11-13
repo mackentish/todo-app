a12b0c941ce25e05839aef4b9f8485d092199ea8
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2025-11-12 21:45:57 -0500

    make header

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ function App() {
     const { getAllItems } = useItems();
 
     return (
-        <div className="bg-gray-100 dark:bg-gray-900 w-full h-screen overflow-y-auto p-4">
-            <div className="flex flex-col w-full max-w-3xl items-center mx-auto">
-                <div className="flex flex-col gap-4 mb-8 w-full">
-                    <Text className="text-3xl font-bold">Todo App</Text>
-                    <Text className="text-md text-grey-700 dark:text-gray-300">
-                        Manage your tasks efficiently and stay organized!
-                    </Text>
-                </div>
+        <div className="bg-gray-100 dark:bg-gray-900 w-full h-screen overflow-y-auto">
+            <div className="flex flex-col gap-2 mb-8 w-full p-4 bg-white dark:bg-gray-800 shadow-md">
+                <Text className="text-3xl font-bold">Todo App</Text>
+                <Text className="text-md text-grey-700 dark:text-gray-300">
+                    Manage your tasks efficiently and stay organized!
+                </Text>
+            </div>
+            <div className="flex flex-col w-full max-w-3xl items-center mx-auto px-4">
                 <ListView items={getAllItems()} />
             </div>
         </div>
