9a02f2ce53a1da858e51d9982a1b19bc6e0db769
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2025-11-12 21:35:11 -0500

    fix scrolling issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     const { getAllItems } = useItems();
 
     return (
-        <div className="bg-gray-100 min-h-screen dark:bg-gray-900 w-full p-4">
+        <div className="bg-gray-100 dark:bg-gray-900 w-full h-screen overflow-y-auto p-4">
             <div className="flex flex-col w-full max-w-3xl items-center mx-auto">
                 <div className="flex flex-col gap-4 mb-8 w-full">
                     <Text className="text-3xl font-bold">Todo App</Text>
