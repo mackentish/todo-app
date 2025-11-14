deeb0cd01d6d4daf8f0bbf5afd3340da3e95a5f7
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2025-11-13 20:14:30 -0500

    light mode and scroll fix

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -6,7 +6,7 @@ import { HiOutlineTrash, HiOutlinePencil, HiOutlineCheck, HiOutlineX } from 'rea
 
 export const ItemContainerClass = [
     'flex flex-row w-full gap-4 items-center',
-    'rounded-lg border border-gray-200 dark:border-gray-700 px-4 py-2',
+    'rounded-lg border border-gray-300 dark:border-gray-700 px-4 py-2',
 ].join(' ');
 
 export function ItemComponent({ item }: { item: Item }) {
