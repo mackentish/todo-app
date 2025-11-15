6ea44f05f7cb3653d6fe8466868345224a391814
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2025-11-15 08:19:11 -0500

    add empty state

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -24,6 +24,11 @@ export function ListView({ items, includeAddButton = true }: ListViewProps) {
 
     return (
         <div className="flex flex-col w-full gap-2">
+            {items.length === 0 && (
+                <Text className="self-center italic text-gray-500 dark:text-gray-400">
+                    No items found
+                </Text>
+            )}
             {items.map((item) => (
                 <ItemComponent item={item} />
             ))}
